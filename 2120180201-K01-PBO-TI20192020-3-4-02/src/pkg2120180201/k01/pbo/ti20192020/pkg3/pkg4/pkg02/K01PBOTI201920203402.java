f74b9f8448fbccdd9bf4eef97930cb6e79514caa
ghurotul <ghurotul.br@gmail.com>
2019-10-22 13:44:52 +0700

    Upload Tugas Kuis01

---
+++ 2120180201-K01-PBO-TI20192020-3-4-02/src/pkg2120180201/k01/pbo/ti20192020/pkg3/pkg4/pkg02/K01PBOTI201920203402.java
@@ -0,0 +1,30 @@
+package pkg2120180201.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class K01PBOTI201920203402 {
+    public static void main(String[] args) {
+    //intansiasi dari kelas mahasiswa
+        //membuat objek teman dari kelas mahasiswa
+       mahasiswa teman=new mahasiswa();
+       //membuat objek teman1 dari kelas mahasiswa
+       mahasiswa teman1=new mahasiswa();
+       //membuat objek teman2 dari kelas mahasiswa
+       mahasiswa teman2=new mahasiswa();
+       //membuat objek teman3 dari kelas mahasiswa
+       mahasiswa teman3=new mahasiswa();
+       //membuat objek teman4 dari kelas mahasiswa
+       mahasiswa teman4=new mahasiswa();
+       
+       System.out.println("Biodata Mahasiswa    :\n");//memunculkan tulisan "Biodata Mahasiswa  :"
+       //diatas hasil dari akses method
+       
+    //akses method
+       //memasukkan biodata  5 teman dan memanggil method
+       teman.tampilBiodata(" Siti Lutfiatun N.", " 2120180202", " Tuban\n");
+       teman1.tampilBiodata(" Suci Nur Ivayanti", " 2120180205", " Bojonegoro\n");
+       teman2.tampilBiodata(" Rahma Melyana R.", " 2120180198", " Bojonegoro\n");
+       teman3.tampilBiodata(" Aulia Rahmah N.N.", " 2120180139", " Bojonegoro\n");
+       teman4.tampilBiodata(" Ulfatun Naela", " 2120180207", " Tuban\n");
+       
+    
+    }
+    
+}
---
+++ 2120180201-K01-PBO-TI20192020-3-4-02/src/pkg2120180201/k01/pbo/ti20192020/pkg3/pkg4/pkg02/mahasiswa.java
@@ -0,0 +1,13 @@
+package pkg2120180201.k01.pbo.ti20192020.pkg3.pkg4.pkg02;
+public class mahasiswa {
+    //atribut class mahasiswa yaitu nama, nim, alamat. dengan type data String dan bersifat public
+    public String nama  ="";
+    public String nim   ="";
+    public String alamat="";
+    //deklarasi method dengan type method String
+    public void tampilBiodata(String nama, String nim, String alamat){
+     System.out.println("Nama   ="+nama);
+     System.out.println("Nim    ="+nim);
+     System.out.println("Alamat ="+alamat);
+    }
+}
